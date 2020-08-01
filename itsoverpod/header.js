98211fce267b82591522bba3955f92a390fe907c
Richard Shu <richardjshu@gmail.com>
2020-08-01 14:26:23 -0700

    Abstract out header, footer, podcast distribution links, & metadata

---
+++ itsoverpod/header.js
@@ -0,0 +1,26 @@
+document.write(`
+	<header id="header">
+		<div class="inner">
+			<!-- Logo -->
+			<a href="index" class="logo">
+				<span class="symbol"><img src="images/logo.png" alt="It's Over Pod Logo" /></span><span class="title">It's Over Pod</span>
+			</a>
+
+			<!-- Nav -->
+			<nav>
+				<ul>
+					<li><a href="#menu">Menu</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+
+	<nav id="menu">
+		<h2>Menu</h2>
+		<ul>
+			<li><a href="index">Home</a></li>
+			<li><a href="about">About</a></li>
+			<li><a href="episodes">Episodes</a></li>
+		</ul>
+	</nav>
+`);
---
+++ itsoverpod/metadata.js
@@ -0,0 +1,24 @@
+document.write(`
+	<meta charset="utf-8" />
+	<meta name="author" 	 content="It's Over Podcast" />
+	<meta name="keywords" 	 content="it's over podcast, its over podcast, it's over pod, its over pod" />
+	<meta name="description" content="Two guys from sunny LA bring on their friends for fun and casual conversations about their hobbies, career paths, and more." />
+	<meta name="viewport"	 content="width=device-width, initial-scale=1, user-scalable=no" />
+
+	<!-- Open Graph meta tags = control how URLs are displayed when shared on social media (e.g. Facebook, Twitter, LinkedIn) --> 
+	<meta property="og:site_name" 	 content="It's Over Podcast" />
+	<meta property="og:title" 		 content="It's Over Podcast" />
+	<meta property="og:url" 		 content="http://itsoverpod.com/" />
+	<meta property="og:type" 		 content="website" />
+	<meta property="og:locale" 		 content="en_US" />
+	<meta property="og:description"  content="Two guys from sunny LA bring on their friends for fun and casual conversations about their hobbies, career paths, and more." />
+	<meta property="og:image" 		 content="http://itsoverpod.com/images/logo.png" />
+	<meta property="og:image:width"  content="500" />
+	<meta property="og:image:height" content="500" />
+
+	<!-- Favicon -->
+	<link rel="shortcut icon" type="image/png" href="images/logo.png" />
+
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+`);
